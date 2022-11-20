33961cecfba45dbd8a1fa6aeeccbb72f00f12e5e
alaafat7albab <alaa.fathalbab@gmail.com>
2022-11-20 02:08:44 +0200

    slider section

--- lib/bmi_screen.dart
+++ lib/bmi_screen.dart
@@ -92,8 +92,62 @@ class _BMIScreenState extends State<BMIScreen> {
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.yellow,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(
+                    20.0,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Height',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: [
+                        Text(
+                          '180',
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Text(
+                          'CM',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      value: 150,
+                      min: 100,
+                      max: 220,
+                      onChanged: (value) {
+                        print(value);
+                      }
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
           Expanded(
